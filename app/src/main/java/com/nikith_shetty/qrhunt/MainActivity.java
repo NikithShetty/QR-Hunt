7012f28fb36813bf5e51f26fab46fdc21df9631f
nikhilnagaraju <nikhilnagaraju96@gmail.com>
2017-02-28 13:23:37 +0530

    implemented admob Interstitial ad

--- app/src/main/java/com/nikith_shetty/qrhunt/MainActivity.java
+++ app/src/main/java/com/nikith_shetty/qrhunt/MainActivity.java
@@ -2,6 +2,7 @@ package com.nikith_shetty.qrhunt;
 
 import android.content.Intent;
 import android.database.Cursor;
+import android.os.Handler;
 import android.support.v4.widget.SlidingPaneLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,16 +17,47 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
 import com.sothree.slidinguppanel.SlidingUpPanelLayout;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     Button scanBtn, genBtn, histBtn;
+    InterstitialAd mInterstitialAd;
+    private InterstitialAd interstitial;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //interstial ad space
+                AdRequest adRequests = new AdRequest.Builder().build();
+                // Prepare the Interstitial Ad
+                interstitial = new InterstitialAd(MainActivity.this);
+// Insert the Ad Unit ID
+                interstitial.setAdUnitId(getString(R.string.admob_interstitial_id));
+                interstitial.loadAd(adRequests);
+// Prepare an Interstitial Ad Listener
+                interstitial.setAdListener(new AdListener() {
+                    public void onAdLoaded() {
+// Call displayInterstitial() function
+                        if (interstitial.isLoaded()) {
+                            interstitial.show();
+                        }
+                    }
+                });
+//interstital finished
+                //Do something after 10s
+            }
+        }, 10000);
 
         scanBtn = (Button) findViewById(R.id.scanBtn);
         genBtn = (Button) findViewById(R.id.generateBtn);
