15d80929c9a5df2839beb896b4165a597df074cf
nikhilnagaraju <nikhilnagaraju96@gmail.com>
2017-03-02 02:53:24 +0530

    minor ui changes

--- app/src/main/java/com/nikith_shetty/qrhunt/AboutActivity.java
+++ app/src/main/java/com/nikith_shetty/qrhunt/AboutActivity.java
@@ -7,16 +7,21 @@ import android.os.Bundle;
 import android.text.Html;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class AboutActivity extends AppCompatActivity implements OnClickListener{
 
     TextView myEmail, nikEmail;
+    ImageButton github;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        github=(ImageButton)findViewById(R.id.github);
+        github.setOnClickListener(this);
 
         myEmail = (TextView) findViewById(R.id.myEmail);
         myEmail.setText(Html.fromHtml(getString(R.string.myEmail)));
@@ -31,9 +36,14 @@ public class AboutActivity extends AppCompatActivity implements OnClickListener{
         if(view.equals(myEmail)){
             Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.parse("mailto:shettynikith3@gmail.com"));
             startActivity(emailIntent);
-        }else if(view.equals(nikEmail)){
+        }if(view.equals(nikEmail)){
             Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.parse("mailto:nikhilnagaraju96@gmail.com"));
             startActivity(emailIntent);
+        }if(view.equals(github)){
+            //github link of project
+            Uri uri = Uri.parse(getString(R.string.gitlink));
+            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+            startActivity(intent);
         }
     }
 }
--- app/src/main/java/com/nikith_shetty/qrhunt/HelpActivity.java
+++ app/src/main/java/com/nikith_shetty/qrhunt/HelpActivity.java
@@ -9,5 +9,7 @@ public class HelpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+
+        //TODO setup a webview using github.io page
     }
 }
